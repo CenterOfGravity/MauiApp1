8d567ca3d20daeb45186c53aa5b4624143a22940
jhanif.kara <jhanif.kara@outlook.com>
2022-12-15 21:15:28 -0500

    Add project files.

---
+++ MauiApp1/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.ViewModels
+{
+    internal class MainPageViewModel
+    {
+        public string Suma(string A, string B)
+        {
+            string Resultado = (Int64.Parse(A) + Int64.Parse(B)).ToString();
+            return Resultado;
+        }
+
+        public string Resta(string A, string B)
+        {
+            string Resultado = (Int64.Parse(A) - Int64.Parse(B)).ToString();
+            return Resultado;
+        }
+    }
+}
