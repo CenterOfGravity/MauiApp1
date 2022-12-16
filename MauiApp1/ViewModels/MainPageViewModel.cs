fe743884abac87c11fc265f410128595718e92ce
jhanif.kara <jhanif.kara@outlook.com>
2022-12-15 22:51:33 -0500

    MVVM_YES_BINDING

--- MauiApp1/ViewModels/MainPageViewModel.cs
+++ MauiApp1/ViewModels/MainPageViewModel.cs
@@ -1,11 +1,14 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using System.Diagnostics;
 
 namespace MauiApp1.ViewModels
 {
     [INotifyPropertyChanged]
     internal partial class MainPageViewModel 
     {
+        Stopwatch stopwatch = new Stopwatch();
+
         [ObservableProperty]
         string a;
 
@@ -18,18 +21,27 @@ namespace MauiApp1.ViewModels
         [ObservableProperty]
         string result;
 
+        [ObservableProperty]
+        string timer;
+
         [RelayCommand]
         void AddBtn()
         {
+            stopwatch.Restart();
             LbLSign = "+";
             Result = (Int64.Parse(a) + Int64.Parse(b)).ToString();
+            Timer = (stopwatch.Elapsed*1000).ToString();
+            stopwatch.Stop();
         }
 
         [RelayCommand]
         void SubtractBtn()
         {
+            stopwatch.Restart();
             LbLSign = "-";
             Result = (Int64.Parse(a) - Int64.Parse(b)).ToString();
+            Timer = (stopwatch.Elapsed * 1000).ToString();
+            stopwatch.Stop();
         }
 
 
